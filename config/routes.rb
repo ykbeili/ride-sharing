a63d43e25cab3d5dd1a0863b1661b5ef61ea2567
ykbeili <ykbeili@gmail.com>
2024-04-26 21:48:49 -0600

    remove devise employe table

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :employees
   devise_for :organizations
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
